60c0b396e23de1b38802cadc388072562f3398bc
Kahar Erbol <kaharerbol@gmail.com>
2023-03-15 12:39:27 -0700

    updating front end, adding some new features

--- ghi/app/src/MainPage.js
+++ ghi/app/src/MainPage.js
@@ -1,16 +1,29 @@
 import { useState, useEffect } from "react";
+import { NavLink } from "react-router-dom";
 
 function MainPage() {
 
   const [pics, setPics] = useState([]);
+  const [backImg, setBackImg] = useState('');
   const fetchData = async () => {
     const url = 'http://localhost:8100/api/models/';
-        const response = await fetch(url);
-        if (response.ok) {
-            const data = await response.json();
-            setPics(data.models);
-        }
+    const response = await fetch(url);
+    if (response.ok) {
+        const data = await response.json();
+        setPics(data.models);
+    }
+
+    const backImgUrl = 'http://localhost:8100/api/models/6';
+    const imgRespose = await fetch(backImgUrl);
+    if (imgRespose.ok) {
+      const data = await imgRespose.json();
+      setBackImg(data.picture_url);
+
+    }
+
   }
+
+
 
   useEffect(() => {
     fetchData();
@@ -23,10 +36,22 @@ function MainPage() {
       <h1 className="display-5 fw-bold">CarCar</h1>
       <div className="col-lg-6 mx-auto">
         <p className="lead mb-4">
-          The premiere solution for automobile dealership
-          management!
+          Vintage Car Service and Sales
         </p>
+        <img src={backImg} className="img-fluid" alt="Backgrounf Image"/>
       </div>
+      <div className="row row-cols-1 row-cols-md-2 g-4">
+          <div className="col">
+            <div className="card" >
+              <img src="..." className="card-img-top" alt="..." />
+              <div className="card-body">
+                <h5 className="card-title">Card title</h5>
+                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" className="btn btn-primary">Go somewhere</a>
+              </div>
+            </div>
+          </div>
+        </div>
     </div>
     </>
   );
--- ghi/app/src/ModelList.js
+++ ghi/app/src/ModelList.js
@@ -19,6 +19,24 @@ class ModelList extends React.Component {
         }
     }
 
+    
+    async handleCancelAppointment(modelId) {
+        const url = `http://localhost:8100/api/models/${modelId}`;
+        const response = await fetch(url, {
+            method: 'DELETE'
+        })
+        if (response.ok) {
+            this.handleAppointment(modelId);
+        }
+
+    }
+
+    handleAppointment(id) {
+        const newModels = this.state.models.filter(model => model.id !== id);
+        this.setState({models: newModels});
+    }
+
+
     render() {
         return (
             <div>
@@ -30,16 +48,22 @@ class ModelList extends React.Component {
                             <th>Model</th>
                             <th>Manufacturer</th>
                             <th>Picture</th>
+                            <th></th>
                         </tr>
                     </thead>
                     <tbody>
                         {this.state.models.map(model => {
                             return (
-                                <tr key={model.id}>
+                                <tr key={model.id} value={model.id}>
                                     <td>{ model.id }</td>
                                     <td>{ model.name }</td>
                                     <td>{ model.manufacturer.name }</td>
                                     <td><img src={model.picture_url}  width="200" height="150" /></td>
+                                    <td>
+                                        <div className="btn-group" role="group" aria-label="Basic mixed styles example">
+                                            <button type="button" className="btn btn-danger" onClick={() => this.handleCancelAppointment(model.id)} >Delete</button>
+                                        </div>
+                                    </td>
                                 </tr>
                             );
                         })}
